bf9d46ad0e42fc0922ff066356d82e054f161ff3
Sota Makino <sotamaki0421@gmail.com>
2021-10-23 03:03:49 +0900

    カレンダーに予定を作成する機能を追加

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function doGet() {
   // 表示したいHTMLのファイル名を指定(拡張子は不要)
   return HtmlService.createTemplateFromFile("index").evaluate();
 }
+
+function doPost() {
+  
+}
--- reserve.js
+++ reserve.js
@@ -1,3 +1,18 @@
-function myFunction() {
-  
+function reserve() {
+    registCalenderEvent(name, myCalendar);
 }
+
+function registCalenderEvent(name, myCalendar) {
+    //アクセス可能なカレンダーのIDを指定して、Googleカレンダーを取得する
+    
+    //Googleカレンダーに登録する日付を設定する
+    let startTime = new Date();
+    let endTime = new Date();
+    let AddTime = 1;
+    //createEventメソッドによる予定登録
+    endTime.setHours(startTime.getHours() + AddTime)
+    myCalendar.createEvent('予約1：' + name, startTime, endTime);
+
+}
+
+
